ec58f322ac5583ab7fe93b2c955fa86247d4b24c
Sukrimardana <142938999+Sukrimardana@users.noreply.github.com>
2024-07-22 22:47:29 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+document.getElementById('showCardBtn1').addEventListener('click', function() {
+  document.getElementById('card1').style.display = 'block';
+});
+
+document.getElementById('closeCardBtn1').addEventListener('click', function() {
+  document.getElementById('card1').style.display = 'none';
+});
+
+//showCarBtn2//
+
+document.getElementById('showCardBtn2').addEventListener('click', function() {
+  document.getElementById('card2').style.display = 'block';
+});
+
+document.getElementById('closeCardBtn2').addEventListener('click', function() {
+  document.getElementById('card2').style.display = 'none';
+})
+
+//showCardBtn3//
+
+document.getElementById('showCardBtn3').addEventListener('click', function() {
+  document.getElementById('card3').style.display = 'block';
+});
+
+document.getElementById('closeCardBtn3').addEventListener('click', function() {
+  document.getElementById('card3').style.display = 'none';
+})
